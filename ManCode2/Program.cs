3083403d2f758df1f8f71c409009527da8707b82
ManTrinh <trinhthongman96@gmail.com>
2021-06-08 16:49:51 +0700

    First Update

---
+++ ManCode2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Name of Junior Dev {0}", "ManTrinh");
+        }
+    }
+}
